d7efdcb73da7b45c124d88a19fb8e3627ab8e8c3
zhenhui <jakon2010sn@gmail.com>
2013-12-06 13:53:37 +0800

    添加视界时间轴

---
+++ axure/lin/data/sitemap.js
@@ -0,0 +1,8 @@
+﻿var sitemap = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h)]);}; 
+var b="rootNodes",c="pageName",d="视界plus",e="type",f="Wireframe",g="url",h="__plus.html";
+return _creator();
+})();
