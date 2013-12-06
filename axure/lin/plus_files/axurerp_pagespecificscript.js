ccdbea3407f72231695296c12f14597c83f19df1
zhenhui <jakon2010sn@gmail.com>
2013-12-06 14:26:43 +0800

    again

---
+++ axure/lin/plus_files/axurerp_pagespecificscript.js
@@ -0,0 +1,16 @@
+﻿for(var i = 0; i < 48; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u45'] = 'top';HookHover('u28', false);
+gv_vAlignTable['u29'] = 'top';HookHover('u8', false);
+gv_vAlignTable['u30'] = 'top';gv_vAlignTable['u35'] = 'top';HookHover('u13', false);
+gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u15'] = 'top';HookHover('u43', false);
+gv_vAlignTable['u44'] = 'top';gv_vAlignTable['u4'] = 'top';gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u39'] = 'top';HookHover('u38', false);
+gv_vAlignTable['u9'] = 'top';gv_vAlignTable['u40'] = 'top';HookHover('u3', false);
+HookHover('u23', false);
+gv_vAlignTable['u24'] = 'top';gv_vAlignTable['u25'] = 'top';gv_vAlignTable['u47'] = 'top';HookHover('u18', false);
+gv_vAlignTable['u19'] = 'top';gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u5'] = 'top';gv_vAlignTable['u46'] = 'top';HookHover('u33', false);
+gv_vAlignTable['u34'] = 'top';
