9aad966171e632bbb9bf2cbfa420584713139f93
zhenhui <jakon2010sn@gmail.com>
2013-12-06 16:18:18 +0800

    qiehuan

--- axure/lin/data/sitemap.js
+++ axure/lin/data/sitemap.js
@@ -3,6 +3,6 @@
 (function() {
     var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
     var _creator = function() { return _(b,[_(c,d,e,f,g,h)]);}; 
-var b="rootNodes",c="pageName",d="视界plus",e="type",f="Wireframe",g="url",h="__plus.html";
+var b="rootNodes",c="pageName",d="视界时间轴",e="type",f="Wireframe",g="url",h="视界时间轴.html";
 return _creator();
 })();
